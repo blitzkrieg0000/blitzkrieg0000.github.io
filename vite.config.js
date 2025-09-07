dc7392c868bc203f90d3beaec276699a7a31e716
Burakhan Şamlı <burakhansamli@proton.me>
2025-09-07 21:04:04 +0300

    .

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/blitzkrieg0000.github.io",
-  
+  // base: "/blitzkrieg0000.github.io"
 })
