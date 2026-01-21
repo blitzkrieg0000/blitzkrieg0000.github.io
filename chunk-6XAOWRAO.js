d88bc32d74b6341863ad280cd34be4356b4d2904
Burakhan Şamlı <burakhansamli@proton.me>
2026-01-21 11:14:37 +0300

    .

---
+++ chunk-6XAOWRAO.js
@@ -0,0 +1,11 @@
+import "./chunk-653SOEEV.js";
+
+// src/environments/environment.development.ts
+var environment = {
+  production: false,
+  telephone: "0.0.0.0"
+};
+export {
+  environment
+};
+//# sourceMappingURL=chunk-6XAOWRAO.js.map
